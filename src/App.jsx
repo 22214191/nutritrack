cd1d5541b32ad7f8a38a06ffc2248a1b922d417c
Abdalla <abdallaibrahim850@gmail.com>
2026-05-08 02:33:52 +0300

    fix routing and CSP

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@
 // =============================================
 
 import { QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { queryClientInstance } from "@/lib/queryClient";
 
 import AppLayout  from "@/components/layout/AppLayout";
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
 export default defineConfig({
   base: '/nutritrack/',
+  build: {
+    target: 'esnext'
+  },
   plugins: [react()],
   resolve: {
     alias: {
